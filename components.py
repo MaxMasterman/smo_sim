6da731c50559ecddc0a09eccfd194d5089e46aae
MaxMasterman <55034735+MaxMasterman@users.noreply.github.com>
2019-11-24 22:07:35 +0100

    Update components.py

--- components.py
+++ components.py
@@ -23,6 +23,54 @@ class Signal:
     def sin(self, t):
         return self.amplitude*(np.sin(2*np.pi*t/self.period))
     
+
+class Pump:
+    
+    """ TUDOS pump from FH slides """
+    
+    def __init__(self):
+        self.diameter = 5.7*10**-3 # m
+        self.A = np.pi*self.diameter/4
+        
+        self.zmax = 35*10**-6 # m ANNAHME
+        self.zmin = -15*10**-6 # m ANNAHME
+        
+        self.Vmax = 240 # V operation voltage
+        self.Vmin = -76 # V operation voltage
+        self.Pmax = 50 # kPa back pressure air
+        self.Pmin = -38 # kPa suction pressure air
+        
+        self.Rs = 1 # spezifische Gaskonstante
+        self.T = 293 # K
+        
+    def stroke(self, voltage):
+        if voltage > 0:
+            if voltage > self.Vmax:
+                return self.zmax
+            else:
+                return self.zmax*voltage/self.Vmax
+        if voltage < 0:
+            if voltage < self.Vmin:
+                return self.zmin
+            else:
+                return self.zmin*voltage/self.Vmin
+        
+    def capacity(self, voltage):
+        # C = A*z/(Rs*T)
+        return self.A*self.stroke(voltage)/(self.Rs*self.T)
+    
+    def pressure(self, voltage):
+        if voltage > 0:
+            if voltage > self.Vmax:
+                return self.Pmax
+            else:
+                return self.Pmax*voltage/self.Vmax
+        if voltage < 0:
+            if voltage < self.Vmin:
+                return self.Pmin
+            else:
+                return self.Pmin*voltage/self.Vmin
+    
     
 class Velve:
     
@@ -42,16 +90,17 @@ class Velve:
         if u > 0: # druckhub
             return self.R
         else: # saughub
-            return self.R*10**2 # leckstrom 
+            return self.R*10**9 # leckstrom 
     
 
 if __name__ == '__main__':
     
     import pandas as pd
     import matplotlib.pyplot as plt
-    
+
     fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
     
+    # vizualize signals amplitudes
     signal = Signal(amplitude=5, frequency=1)
     curves = pd.DataFrame(columns=['t', 'sin', 'rect'])
     curves['t'] = np.linspace(0, 3, 251)
@@ -61,6 +110,7 @@ if __name__ == '__main__':
     ax1.set_xlabel('Time [s]')
     ax1.set_ylabel('Voltage [V]')
     
+    # vizualize velve resistance
     velve = Velve(R=1)
     curves = pd.DataFrame(columns=['u', 'v_fw', 'v_bw'])
     curves['u'] = np.linspace(-1, 1, 251)
@@ -71,18 +121,19 @@ if __name__ == '__main__':
     ax2.set_xlabel('Voltage [V]')
     ax2.set_ylabel('Resistance [Ohm]')
     
-
+    fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1)
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    # visualize pump pressure and capacity
+    pump = Pump()
+    curves = pd.DataFrame(columns=['vcc', 'pressure'])
+    curves['vcc'] = np.linspace(-100, 300, 251)
+    curves['pressure']  = [pump.pressure(v)  for v in curves['vcc']]
+    curves.plot(x='vcc', y='pressure', ax=ax1, grid=True)
+    ax1.set_xlabel('Voltage [V]')
+    ax1.set_ylabel('Pressure [kPa]')
+    curves = pd.DataFrame(columns=['vcc', 'capacity'])
+    curves['vcc'] = np.linspace(-100, 300, 251)
+    curves['capacity']  = [pump.capacity(v)  for v in curves['vcc']]
+    curves.plot(x='vcc', y='capacity', ax=ax2, grid=True)
+    ax2.set_xlabel('Voltage [V]')
+    ax2.set_ylabel('Pressure [m³/kPa]')
